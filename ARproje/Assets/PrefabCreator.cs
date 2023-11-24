81f2b2baf6f7651598a6500e67a3b1b324463621
ilhncanan <simsekmekkuin32@hotmail.com>
2023-11-24 13:50:45 +0300

    ARcoreproje

---
+++ ARproje/Assets/PrefabCreator.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PrefabCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject dragonPrefab;
+    [SerializeField] private Vector3 prefabOffset;
+
+    private GameObject dragon;
+    private ARTrackedImageManager ARTrackedImageManager;
+
+    private void OnEnable()
+    {
+        ARTrackedImageManager =gameObject.gameObject.GetComponent<ARTrackedImageManager>();
+
+        ARTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs obj)
+    {
+        foreach (ARTrackedImage image in obj.added)
+        {
+            dragon = Instantiate(dragonPrefab, image.transform);
+            dragon.transform.position += prefabOffset;
+
+        }
+    }
+}
